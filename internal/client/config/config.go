f29094d9a0ca2d14b96cbece92695ff0134c27bf
Pavel Utkin <pavel.utkin@abbyy.com>
2023-09-04 22:52:17 +0300

    server configs

--- internal/client/config/config.go
+++ internal/client/config/config.go
@@ -8,14 +8,21 @@ import (
 )
 
 type ConfigClient struct {
-	GRPC       string       `env:"GRPC" envDefault:"localhost:8080"`
+	GRPC       string       `env:"AddressGRPC"`
 	DebugLevel logrus.Level `env:"DEBUG_LEVEL" envDefault:"debug"`
+	FileFolder string       `env:"DATA_FOLDER"`
+	FileSize   int          `env:"FILE_SIZE"`
 }
 
 // NewConfigClient - creates a new instance with the configuration for the client
 func NewConfig() *ConfigClient {
-	configClient := ConfigClient{}
+	configClient := ConfigClient{
+		GRPC:       "localhost:8080",
+		FileFolder: "./data/client_keeper",
+		FileSize:   4000000,
+	}
 	flag.StringVar(&configClient.GRPC, "g", configClient.GRPC, "Server address")
+	flag.StringVar(&configClient.FileFolder, "f", configClient.FileFolder, "File Folder")
 	flag.Parse()
 	err := env.Parse(&configClient)
 	if err != nil {
